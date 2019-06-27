8d7df1c35832297bdedcc47a9978a61fcf747cfa
Alexander Fedorchenko <smolnax@gmail.com>
2019-06-28 00:22:39 +0300

    edit to HW2 "H/W  Python functionaly"

--- flask_lesson_3/news/marshal_structure.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import fields
-
-news_structure = {
-    "id": fields.Integer,
-    "title": fields.String,
-    "text": fields.String,
-    "date": fields.DateTime
-}
--- relational_database/homework.py
+++ relational_database/homework.py
@@ -12,46 +12,31 @@ def task_1_add_new_record_to_db(con) -> None:
         'postalcode': '774',
         'country': 'Singapore',
     }
-
     Args:
         con: psycopg connection
-
     Returns: 92 records
-
     """
-    with con.cursor() as cur:
-        cur.execute("INSERT INTO Customers (customername, contactname, address, city, postalcode, country) VALUES ("
-                "'Thomas', 'David', 'Some Address', 'London', '774', 'Singapore');")
-
-        cur.execute("SELECT * FROM CUSTOMERS")
-        cur.fetchall()
+    pass
 
 
 def task_2_list_all_customers(cur) -> list:
     """
     Get all records from table Customers
-
     Args:
         cur: psycopg cursor
-
     Returns: 91 records
-
     """
-    cur.execute("SELECT * FROM customers;")
-    return cur.fetchall()
+    pass
 
 
 def task_3_list_customers_in_germany(cur) -> list:
     """
     List the customers in Germany
-
     Args:
         cur: psycopg cursor
-
     Returns: 11 records
     """
-    cur.execute("SELECT * FROM Customers WHERE Country='germany';")
-    return cur.fetchall()
+    pass
 
 
 def task_4_update_customer(con):
@@ -59,175 +44,123 @@ def task_4_update_customer(con):
     Update first customer's name (Set customername equal to  'Johnny Depp')
     Args:
         cur: psycopg cursor
-
     Returns: 91 records with updated customer
-
     """
-    with con.cursor() as cur:
-        cur.execute("UPDATE Customers SET customername = 'Johnny Depp' WHERE customerid=1 ")
-        cur.execute("SELECT * FROM customers;")
-        cur.fetchall()
-
+    pass
 
 
 def task_5_delete_the_last_customer(con) -> None:
     """
     Delete the last customer
-
     Args:
         con: psycopg connection
     """
-    with con.cursor() as cur:
-        cur.execute("DELETE FROM Customers WHERE customerid in (SELECT MAX (customerid) FROM Customers); ")
-        #cur.execute("SELECT * FROM customers;")
-        #cur.fetchall()
+    pass
 
 
 def task_6_list_all_supplier_countries(cur) -> list:
     """
     List all supplier countries
-
     Args:
         cur: psycopg cursor
-
     Returns: 29 records
-
     """
-    cur.execute("SELECT country FROM Suppliers;")
-    return cur.fetchall()
+    pass
 
 
 def task_7_list_supplier_countries_in_desc_order(cur) -> list:
     """
     List all supplier countries in descending order
-
     Args:
         cur: psycopg cursor
-
     Returns: 29 records in descending order
-
     """
-    cur.execute("SELECT country FROM Suppliers ORDER BY country DESC;")
-    return cur.fetchall()
+    pass
 
 
 def task_8_count_customers_by_city(cur):
     """
     List the number of customers in each city
-
     Args:
         cur: psycopg cursor
-
     Returns: 69 records in descending order
-
     """
-    cur.execute("SELECT COUNT(customerid), City FROM Customers GROUP BY City ORDER BY city DESC;")
-    return cur.fetchall()
+    pass
 
 
 def task_9_count_customers_by_country_with_than_10_customers(cur):
     """
     List the number of customers in each country. Only include countries with more than 10 customers.
-
     Args:
         cur: psycopg cursor
-
     Returns: 3 records
     """
-    cur.execute("SELECT COUNT(customerid), Country FROM Customers GROUP BY Country HAVING COUNT(customerid) > 10;")
-    return cur.fetchall()
+    pass
 
 
 def task_10_list_first_10_customers(cur):
     """
     List first 10 customers from the table
-
     Results: 10 records
     """
-    #cur.execute("SELECT TOP 10 * FROM Customers;")
-    cur.execute("SELECT * FROM Customers WHERE customerid < 11;")
-    return cur.fetchall()
+    pass
 
 
 def task_11_list_customers_starting_from_11th(cur):
     """
     List all customers starting from 11th record
-
     Args:
         cur: psycopg cursor
-
     Returns: 11 records
     """
-    cur.execute("SELECT * FROM Customers WHERE customerid >= 12;")
-    return cur.fetchall()
+    pass
 
 
 def task_12_list_suppliers_from_specified_countries(cur):
     """
     List all suppliers from the USA, UK, OR Japan
-
     Args:
         cur: psycopg cursor
-
     Returns: 8 records
     """
-    cur.execute("SELECT * FROM Suppliers WHERE country='USA' AND country ='UK' AND country ='Japan';")
-    return cur.fetchall()
+    pass
 
 
 def task_13_list_products_from_sweden_suppliers(cur):
     """
     List products with suppliers from Sweden.
-
     Args:
         cur: psycopg cursor
-
     Returns: 3 records
     """
-    cur.execute("SELECT Products.ProductName FROM Suppliers INNER JOIN Products ON Suppliers.SupplierId = Products.SupplierID WHERE Country ='Sweden';")
-    return cur.fetchall()
+    pass
 
 
 def task_14_list_products_with_supplier_information(cur):
     """
     List all products with supplier information
-
     Args:
         cur: psycopg cursor
-
     Returns: 77 records
     """
-    cur.execute("SELECT Products.ProductID, Products.ProductName, Products.Unit, Products.Price, Country, City, Suppliername FROM Products LEFT JOIN Suppliers ON Suppliers.SupplierId = Products.SupplierID;")
-    return cur.fetchall()
+    pass
 
 
 def task_15_list_customers_with_any_order_or_not(cur):
     """
     List all customers, whether they placed any order or not.
-
     Args:
         cur: psycopg cursor
-
     Returns: 213 records
     """
-    cur.execute(" SELECT CustomerName, ContactName, Country, OrderId " 
-    "FROM Customers LEFT JOIN Orders ON "
-                "Customers.CustomerId = Orders.CustomerId ORDER BY OrderId;")
-    return cur.fetchall()
+    pass
 
 
 def task_16_match_all_customers_and_suppliers_by_country(cur):
     """
     Match all customers and suppliers by country
-
     Args:
         cur: psycopg cursor
-
     Returns: 194 records
     """
-    cur.execute("SELECT Customers.customername, Customers.address, Customers.country as customercountry, "
-                "Suppliers.country as suppliercountry, "
-                "SupplierName " 
-    " FROM Customers FULL JOIN Suppliers ON Customers.Country = Suppliers.Country "
-                " ORDER BY Customers.country, Suppliers.country;")
-    return cur.fetchall()
+pass
